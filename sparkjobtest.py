4be06bd565f4e93d25e3b28d9df92f820fd04619
idan.asher <idan.asher@biocatch.com>
2018-01-01 15:58:36 +0200

    sec

--- sparkjobtest.py
+++ sparkjobtest.py
@@ -1,25 +1,25 @@
 from pyspark.sql import SparkSession
 
+if __name__ == '__main__':
+	spark = SparkSession.builder.appName("SparkSQLIdanTest").getOrCreate()
 
-spark = SparkSession.builder.appName("SparkSQLIdanTest").getOrCreate()
+	person = spark.createDataFrame([
+	(0, "Bill Chambers", 0, [100]),
+	(1, "Matei Zaharia", 1, [500, 250, 100]),
+	(2, "Michael Armbrust", 1, [250, 100])])\
+	.toDF("id", "name", "graduate_program", "spark_status")
+	graduateProgram = spark.createDataFrame([
+	(0, "Masters", "School of Information", "UC Berkeley"),
+	(2, "Masters", "EECS", "UC Berkeley"),
+	(1, "Ph.D.", "EECS", "UC Berkeley")])\
+	.toDF("id", "degree", "department", "school")
+	sparkStatus = spark.createDataFrame([
+	(500, "Vice President"),
+	(250, "PMC Member"),
+	(100, "Contributor")])\
+	.toDF("id", "status")
+	person.createOrReplaceTempView("person")
+	graduateProgram.createOrReplaceTempView("graduateProgram")
+	sparkStatus.createOrReplaceTempView("sparkStatus")
 
-person = spark.createDataFrame([
-(0, "Bill Chambers", 0, [100]),
-(1, "Matei Zaharia", 1, [500, 250, 100]),
-(2, "Michael Armbrust", 1, [250, 100])])\
-.toDF("id", "name", "graduate_program", "spark_status")
-graduateProgram = spark.createDataFrame([
-(0, "Masters", "School of Information", "UC Berkeley"),
-(2, "Masters", "EECS", "UC Berkeley"),
-(1, "Ph.D.", "EECS", "UC Berkeley")])\
-.toDF("id", "degree", "department", "school")
-sparkStatus = spark.createDataFrame([
-(500, "Vice President"),
-(250, "PMC Member"),
-(100, "Contributor")])\
-.toDF("id", "status")
-person.createOrReplaceTempView("person")
-graduateProgram.createOrReplaceTempView("graduateProgram")
-sparkStatus.createOrReplaceTempView("sparkStatus")
-
-spark.sql("select * from person").show()
+	spark.sql("select * from person").show()
